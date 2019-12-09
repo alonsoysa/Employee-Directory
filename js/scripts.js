5a03f2187278298d1992ae30689234519c03bd84
Alonso <alonsoysa@gmail.com>
2019-12-09 15:43:02 -0800

    add 12 random users to the screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Get and display 12 random 
-
+const gallery = document.querySelector('#gallery');
     
 // ------------------------------------------
 //  FETCH FUNCTIONS
@@ -12,7 +12,7 @@ function fetchData(url) {
 }
 
 fetchData('https://randomuser.me/api/?results=12')
-    .then(data => console.log(data));
+    .then(data => generateCards(data.results));
 
 
 // ------------------------------------------
@@ -24,6 +24,36 @@ function checkStatus(response) {
     } else {
         return Promise.reject(new Error(response.statusText));
     }
+}
+
+function generateCards(data) {
+    const html = data.map(item => generateCardHTML(item)).join('');
+    gallery.innerHTML = html;
+}
+
+function generateCardHTML(item) {
+    console.log(item);
+
+    const picURL =  item.picture.large;
+    const firstName = item.name.first
+    const lastName = item.name.last
+    const email = item.email;
+    const city = item.location.city;
+    const state = item.location.state;
+
+    const html = `
+        <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${picURL}" alt="$${firstName} ${lastName} profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
+                <p class="card-text">${email}</p>
+                <p class="card-text cap">${city}, ${state}</p>
+            </div>
+        </div>
+    `;
+    return html;
 }
 
 
