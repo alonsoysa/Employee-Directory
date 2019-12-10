4cfa6f24617a08dcf18d0de0d7dcb9f467ed0fee
Alonso <alonsoysa@gmail.com>
2019-12-09 18:06:01 -0800

    add modals to the document

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // Get and display 12 random 
 const gallery = document.querySelector('#gallery');
+let apiUsers;
     
 // ------------------------------------------
 //  FETCH FUNCTIONS
@@ -12,7 +13,7 @@ function fetchData(url) {
 }
 
 fetchData('https://randomuser.me/api/?results=12')
-    .then(data => generateCards(data.results));
+    .then(data => generateUsers(data.results));
 
 
 // ------------------------------------------
@@ -26,12 +27,28 @@ function checkStatus(response) {
     }
 }
 
-function generateCards(data) {
-    const html = data.map(item => generateCardHTML(item)).join('');
-    gallery.innerHTML = html;
+function generateUsers(data) {
+    // Set the global apiUsers to the current randomUser data
+    apiUsers = data;
+
+    // Add cards to the app
+    const cardsHTML = data.map((item, index) => generateCardHTML(item, index)).join('');
+    gallery.innerHTML = cardsHTML;
+
+    let popupsHTML = data.map((item, index) => generateModalHTML(item, index)).join('');
+
+    popupsHTML = '<div class="modal-container">' + popupsHTML;
+    popupsHTML += `<div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>`;
+    popupsHTML += '</div>';
+
+    // Insert After
+    gallery.insertAdjacentHTML('afterend', popupsHTML);
 }
 
-function generateCardHTML(item) {
+function generateCardHTML(item, index) {
     console.log(item);
 
     const picURL =  item.picture.large;
@@ -42,18 +59,64 @@ function generateCardHTML(item) {
     const state = item.location.state;
 
     const html = `
-        <div class="card">
+        <a class="card" data-id="${index}">
             <div class="card-img-container">
-                <img class="card-img" src="${picURL}" alt="$${firstName} ${lastName} profile picture">
+                <img class="card-img" src="${picURL}" alt="${firstName} ${lastName} profile picture">
             </div>
             <div class="card-info-container">
                 <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
                 <p class="card-text">${email}</p>
                 <p class="card-text cap">${city}, ${state}</p>
             </div>
-        </div>
+        </a>
     `;
     return html;
 }
 
+function generateModalHTML(item, index) {
+    const picURL = item.picture.large;
+    const firstName = item.name.first
+    const lastName = item.name.last
+    const email = item.email;
+    const city = item.location.city;
+    const state = item.location.state;
+    const phone = item.phone;
+    const birthday = item.dob.date;
+    let address = item.location.street.number + ' ' 
+        address += item.location.street.name + ', ';
+        address += city + ', ';
+        address += state + ' ';
+        address += item.location.postcode;
 
+    const html = `
+            <div class="modal" data-id="${index}">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${picURL}" alt="${firstName} ${lastName} profile picture">
+                    <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3>
+                    <p class="modal-text">${email}</p>
+                    <p class="modal-text cap">${city}</p>
+                    <hr>
+                    <p class="modal-text">${phone}</p>
+                    <p class="modal-text">${address}</p>
+                    <p class="modal-text">${birthday}</p>
+                </div>
+            </div>
+        `;
+    
+    return html;
+}
+
+// ------------------------------------------
+//  Listeners
+// ------------------------------------------
+gallery.addEventListener('click', (event) => {
+    let card; 
+    if (event.target.hasAttribute('data-id')) {
+        card = event.target;
+    } else {
+        if ( !event.target.closest('.card') ) return;
+        card = event.target.closest('.card');
+    }
+    console.log(card.getAttribute('data-id'));
+});
