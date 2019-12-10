07f2e332c2b5ef261a277d41e2fa51c9029298c0
Alonso <alonsoysa@gmail.com>
2019-12-10 15:43:59 -0800

    adds state abbreviation function

--- js/scripts.js
+++ js/scripts.js
@@ -62,6 +62,89 @@ function formatBirthday(string) {
     return birthday;
 }
 
+/**
+ * Abbreviates States
+ * https://gist.github.com/calebgrove/c285a9510948b633aa47
+ * Usage
+ * abbrState('ny', 'name'); --> 'New York'
+ * abbrState('New York', 'abbr'); --> 'NY'
+ * 
+ * @param {string} input State
+ * @param {string} to type name or abbr
+ * @returns {string} 
+ */
+function abbrState(input, to) {
+
+    var states = [
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+
+    if (to == 'abbr') {
+        input = input.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+        for (i = 0; i < states.length; i++) {
+            if (states[i][0] == input) {
+                return (states[i][1]);
+            }
+        }
+    } else if (to == 'name') {
+        input = input.toUpperCase();
+        for (i = 0; i < states.length; i++) {
+            if (states[i][1] == input) {
+                return (states[i][0]);
+            }
+        }
+    }
+}
+
 //=============================================================================
 // CARD FUNCTIONS
 //=============================================================================
@@ -104,7 +187,7 @@ function generateCard(user) {
     const lastName = user.name.last
     const email = user.email;
     const city = user.location.city;
-    const state = user.location.state;
+    const state = user.location.state; 
 
     const html = `
         <div class="card">
@@ -192,7 +275,7 @@ function generateSingleModal(user, index, toggle) {
     const lastName = user.name.last
     const email = user.email;
     const city = user.location.city;
-    const state = user.location.state;
+    const state = abbrState(user.location.state, 'abbr');
     const phone = user.phone;
     const birthday = formatBirthday(user.dob.date);
     let address = user.location.street.number + ' ' 
@@ -215,7 +298,7 @@ function generateSingleModal(user, index, toggle) {
                 <hr>
                 <p class="modal-text">${phone}</p>
                 <p class="modal-text">${address}</p>
-                <p class="modal-text">${birthday}</p>
+                <p class="modal-text">Birthday: ${birthday}</p>
             </div>
         </div>
     `;
@@ -267,7 +350,7 @@ function generateForm() {
             triggerSearch(keyword, names);
         }
 
-        // Otherwise wait .3 seconds before executing code.
+        // Otherwise wait .25 seconds before executing code.
         // This prevents from code running too many times
         else {
             if (globalTimeout != null) {
@@ -275,7 +358,7 @@ function generateForm() {
             }
             globalTimeout = setTimeout(() => {
                 triggerSearch(keyword, names);
-            }, 750);
+            }, 250);
         }
         return false;
     });
